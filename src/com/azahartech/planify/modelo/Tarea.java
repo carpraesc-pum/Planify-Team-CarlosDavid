7af0f96376f1e3dcb8bd166ddd73605ebac91fb4
Carlos Prada <carpraesc@alu.edu.gva.es>
2025-11-10 20:41:16 +0100

    Implementar la clase Tarea y lista de tareas(faltaba todo el código de la clase Tarea, en este commit implementada)

--- src/com/azahartech/planify/modelo/Tarea.java
+++ src/com/azahartech/planify/modelo/Tarea.java
@@ -1,5 +1,30 @@
 package com.azahartech.planify.modelo;
 
 public class Tarea {
+    private static int contadorIds = 1; // Variable estática para generar IDs únicos
+    private int id;
+    private String descripcion;
+    private boolean completada;
 
+    public Tarea(String descripcion) {
+        this.id = contadorIds++;
+        this.descripcion = descripcion;
+        this.completada = false; // Por defecto, una nueva tarea no está completada
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
 }
