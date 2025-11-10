74bcdf1a18e26c76ba36184942844665caf14c0c
Carlos Prada <carpraesc@alu.edu.gva.es>
2025-11-10 20:48:38 +0100

    Feat: Implementar la función para añadir una nueva tarea

--- src/com/azahartech/planify/servicio/GestorTareas.java
+++ src/com/azahartech/planify/servicio/GestorTareas.java
@@ -6,4 +6,22 @@ import java.util.List;
 
 public class GestorTareas {
     private List<Tarea> listaDeTareas;
+    public GestorTareas() {
+        this.listaDeTareas = new ArrayList<>();
+    }
+
+    /**
+     * Añade una nueva tarea a la lista.
+     * @param descripcion El texto de la nueva tarea.
+     */
+    public void anadirTarea(String descripcion) {
+        if (descripcion == null || descripcion.trim().isEmpty()) {
+            System.out.println("-> Error: La descripción de la tarea no puede estar vacía.");
+            return;
+        }
+        Tarea nuevaTarea = new Tarea(descripcion);
+        listaDeTareas.add(nuevaTarea);
+        System.out.println("-> Tarea añadida con éxito.");
+    }
+
 }
