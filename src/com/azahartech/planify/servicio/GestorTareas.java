0bd6069e7c11d462f60fa2f52e2ce917d951686c
Carlos Prada <carpraesc@alu.edu.gva.es>
2025-11-13 18:26:55 +0100

    Feat: Implementar la función para marcar una tarea como completada.

--- src/com/azahartech/planify/servicio/GestorTareas.java
+++ src/com/azahartech/planify/servicio/GestorTareas.java
@@ -38,4 +38,20 @@ public class GestorTareas {
         }
         System.out.println("------------------");
     }
+
+    public void marcarTareaComoCompletada(int id) {
+        for (Tarea tarea : listaDeTareas) {
+            if (tarea.getId() == id) {
+                if (tarea.isCompletada()) {
+                    System.out.println("-> La tarea #" + id + " ya estaba completada.");
+                } else {
+                    tarea.setCompletada(true);
+                    System.out.println("-> ¡Bien hecho! Tarea #" + id + " marcada como completada.");
+                }
+                return; // Salimos del método una vez encontrada la tarea
+            }
+        }
+        // Si el bucle termina, la tarea no fue encontrada
+        System.out.println("-> Error: No se encontró ninguna tarea con el ID " + id + ".");
+    }
 }
